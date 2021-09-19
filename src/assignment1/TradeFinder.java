ba037da9e900a669dfc6d9b78bbdc84392f0226e
pranavdhoolia <dhoolia.pranav@gmail.com>
2021-09-20 01:53:41 +0530

    put TraderGraph class in same file

--- src/assignment1/TradeFinder.java
+++ src/assignment1/TradeFinder.java
@@ -52,3 +52,79 @@ public class TradeFinder {
         return false;
     }
 }
+
+final class TraderGraph {
+    // adjacency list
+    public HashMap<Trader, List<Trader>> adj;
+    public TraderGraph() {
+        adj = new HashMap<>();
+    }
+
+    // find BFS path (true)
+    public boolean findPath(Trader src, Trader dest) {
+        if (src == dest) return true;
+
+        HashMap<Trader, Boolean> visited = new HashMap<Trader, Boolean>();
+        LinkedList<Trader> queue = new LinkedList<Trader>();
+
+        // enque current node
+        visited.put(src, true);
+        queue.add(src);
+
+        while (queue.size() != 0) {
+            src = queue.poll();
+
+            // PATH EXISTS !!!
+            if (src == dest) {
+                return true;
+            }
+
+            // Get all adjacent
+            // IF !visited, VISIT AND ENQUE
+            Iterator<Trader> adjacents = adj
+                    .get(src)
+                    .listIterator();
+
+            while (adjacents.hasNext()) {
+                Trader adjacentTrader = adjacents.next();
+                try {
+                    if (!visited.get(adjacentTrader)) {
+                        visited.put(adjacentTrader, true);
+                        queue.add(adjacentTrader);
+                    }
+                } catch (Exception e) {
+                    visited.put(adjacentTrader, true);
+                    queue.add(adjacentTrader);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // add a new vertex
+    public void newVertex(Trader vertex) {
+        adj.put(vertex, new ArrayList<>());
+    }
+
+    // add a new edge
+    public void newEdge(Trader x,
+                        Trader y) {
+
+        // check if an edge is already there
+        try {
+            if (adj.get(x).contains(y) && adj.get(y).contains(x)) return;
+        } catch (NullPointerException e) {
+        }
+
+        // add vertices if not present
+        if (!adj.containsKey(x)) newVertex(x);
+        if (!adj.containsKey(y)) newVertex(y);
+
+        // add undirected edge
+        adj.get(x).add(y);
+        adj.get(y).add(x);
+    }
+
+}
+
--- src/assignment1/test/GraphTest.java
+++ src/assignment1/test/GraphTest.java
@@ -1,74 +1,74 @@
-package assignment1.test;
-
-import assignment1.ItemType;
-import assignment1.Trader;
-import assignment1.TraderGraph;
-import org.junit.*;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-
-import static org.junit.Assert.*;
-
-public class GraphTest {
-
-    static TraderGraph g;
-    static List<Trader> traders;
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        g = new TraderGraph();
-
-        // initialize 5 traders
-        traders = new ArrayList<>();
-        for(int i=1; i<8; i++) {
-            HashSet<ItemType> tradableItems = new HashSet<ItemType>();
-            tradableItems.add(new ItemType(1));
-            traders.add(
-                    new Trader("t"+Integer.valueOf(i), new ItemType(1), tradableItems)
-            );
-        }
-
-        // add 5 vertices
-        for(int i=1; i<7; i++) {
-            g.newVertex(traders.get(i));
-        }
-
-        // add connections
-        g.newEdge(traders.get(1), traders.get(2));
-        g.newEdge(traders.get(1), traders.get(3));
-        g.newEdge(traders.get(2), traders.get(5));
-        g.newEdge(traders.get(4), traders.get(6));
-    }
-
-    @Test
-    public void addEdge() {
-        // add connections
-        System.out.println(
-                g
-        );
-
-        System.out.println(
-                traders
-        );
-
-        g.newEdge(traders.get(1), traders.get(2));
-        g.newEdge(traders.get(1), traders.get(3));
-        g.newEdge(traders.get(2), traders.get(5));
-        g.newEdge(traders.get(4), traders.get(6));
-    }
-
-    @Test
-    public void pathExists() {
-        assertTrue(g.findPath(traders.get(1), traders.get(2)));
-        assertTrue(g.findPath(traders.get(1), traders.get(3)));
-        assertTrue(g.findPath(traders.get(2), traders.get(5)));
-        assertTrue(g.findPath(traders.get(4), traders.get(6)));
-        assertTrue(g.findPath(traders.get(1), traders.get(5)));
-        assertTrue(g.findPath(traders.get(3), traders.get(5)));
-        assertTrue(g.findPath(traders.get(3), traders.get(2)));
-        assertFalse(g.findPath(traders.get(1), traders.get(4)));
-        assertFalse(g.findPath(traders.get(2), traders.get(6)));
-    }
-}
+//package assignment1.test;
+//
+//import assignment1.ItemType;
+//import assignment1.Trader;
+//import assignment1.TraderGraph;
+//import org.junit.*;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.List;
+//
+//import static org.junit.Assert.*;
+//
+//public class GraphTest {
+//
+//    static TraderGraph g;
+//    static List<Trader> traders;
+//
+//    @BeforeClass
+//    public static void setUp() throws Exception {
+//        g = new TraderGraph();
+//
+//        // initialize 5 traders
+//        traders = new ArrayList<>();
+//        for(int i=1; i<8; i++) {
+//            HashSet<ItemType> tradableItems = new HashSet<ItemType>();
+//            tradableItems.add(new ItemType(1));
+//            traders.add(
+//                    new Trader("t"+Integer.valueOf(i), new ItemType(1), tradableItems)
+//            );
+//        }
+//
+//        // add 5 vertices
+//        for(int i=1; i<7; i++) {
+//            g.newVertex(traders.get(i));
+//        }
+//
+//        // add connections
+//        g.newEdge(traders.get(1), traders.get(2));
+//        g.newEdge(traders.get(1), traders.get(3));
+//        g.newEdge(traders.get(2), traders.get(5));
+//        g.newEdge(traders.get(4), traders.get(6));
+//    }
+//
+//    @Test
+//    public void addEdge() {
+//        // add connections
+//        System.out.println(
+//                g
+//        );
+//
+//        System.out.println(
+//                traders
+//        );
+//
+//        g.newEdge(traders.get(1), traders.get(2));
+//        g.newEdge(traders.get(1), traders.get(3));
+//        g.newEdge(traders.get(2), traders.get(5));
+//        g.newEdge(traders.get(4), traders.get(6));
+//    }
+//
+//    @Test
+//    public void pathExists() {
+//        assertTrue(g.findPath(traders.get(1), traders.get(2)));
+//        assertTrue(g.findPath(traders.get(1), traders.get(3)));
+//        assertTrue(g.findPath(traders.get(2), traders.get(5)));
+//        assertTrue(g.findPath(traders.get(4), traders.get(6)));
+//        assertTrue(g.findPath(traders.get(1), traders.get(5)));
+//        assertTrue(g.findPath(traders.get(3), traders.get(5)));
+//        assertTrue(g.findPath(traders.get(3), traders.get(2)));
+//        assertFalse(g.findPath(traders.get(1), traders.get(4)));
+//        assertFalse(g.findPath(traders.get(2), traders.get(6)));
+//    }
+//}
